9430abb298a5c1da585e7151a77ac3a34eecd042
soyjuanmr-create <soyjuanmr@gmail.com>
2026-03-02 22:00:51 +0100

    fix: pass DATABASE_URL explicitly at runtime to PrismaClient

--- server/models/prisma.js
+++ server/models/prisma.js
@@ -1,3 +1,12 @@
-﻿const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+﻿require('dotenv').config();
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient({
+    datasources: {
+        db: {
+            url: process.env.DATABASE_URL,
+        },
+    },
+});
+
 module.exports = prisma;
